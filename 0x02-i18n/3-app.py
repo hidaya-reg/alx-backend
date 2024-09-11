79527cd56c67cdf516c0208014ff35458089a9a6
hidaya <hidaya.regragui@gmail.com>
2024-09-11 11:52:27 +0100

    doc

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -2,13 +2,14 @@
 """
 Basic Flask app
 """
-
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
 
 class Config:
-    """language configuration"""
+    """
+    language configuration
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -21,13 +22,17 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> str:
-    """get the best match from supported languages"""
+    """
+    get the best match from supported languages
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index() -> str:
-    """Default route"""
+    """
+    Default route
+    """
     return render_template('3-index.html')
 
 
