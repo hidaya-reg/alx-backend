f4c701b274f9677f1beedc7cd70c7a5c9d2c9955
hidaya <hidaya.regragui@gmail.com>
2024-10-30 20:53:17 +0100

    task 4

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -19,17 +19,17 @@ class MRUCache(BaseCaching):
         if key is None or item is None:
             return
 
-        if key in self.cache_data:
-            self.cache_data.move_to_end(key)
-        elif len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            mru_key, _ = self.cache_data.popitem(last=True)
-            print(f"DISCARD: {mru_key}")
-        self.cache_data[key] = item
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", mru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key """
-        if key is None or key not in self.cache_data:
-            return None
-
-        self.cache_data.move_to_end(key)
-        return self.cache_data[key]
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        return self.cache_data.get(key, None)
